3b19e8bc50d17c7bc24df3f1280648dd5949ea48
daionvolkov <daionvolkov@gmail.com>
2023-03-09 22:15:09 +0300

    task6 first commit

---
+++ src/Tasks6/exercise1.java
@@ -0,0 +1,26 @@
+package Tasks6;
+
+public class exercise1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Human {
+        private String name;
+        private String surname;
+
+        public String getName() {
+            return name;
+        }
+        public String getSurname() {
+            return surname;
+        }
+    }
+    public static abstract class Client {
+
+    }
+    public static abstract class BankEmployee {
+
+    }
+}
+

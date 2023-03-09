fb2d31381d588b415b682549fb024c46b3ae9e35
daionvolkov <daionvolkov@gmail.com>
2023-03-09 20:09:19 +0300

    task5 09.03

--- src/Tasks/Task5/exercise2.java
+++ src/Tasks/Task5/exercise2.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class exercise2 {
     public static void main(String[] args) {
-        String word = "annn";
+        String word = "leveд";
         boolean res = polydromWord(word);
         System.out.println(res);
     }
--- src/Tasks/Task5/exercise3.java
+++ src/Tasks/Task5/exercise3.java
@@ -3,13 +3,13 @@ package Tasks.Task5;
 public class exercise3 {
     public static void main(String[] args) {
         String text = "Lorem is ipsum dolor is sit amet consectetur  is adipisicing elit";
-        String badWord = "is";
+        String badWord = " is ";
         String res = changeWord(text, badWord);
         System.out.println(res);
     }
 
     public static String changeWord(String text, String badWord) {
-        String new_text = text.replaceAll(badWord, "[вырезано цензурой]");
+        String new_text = text.replaceAll(badWord, " [вырезано цензурой] ");
         return new_text;
     }
 }

ae65109fbf8402aa3068cf0d2ca675adf6b7d3cb
daionvolkov <daionvolkov@gmail.com>
2023-03-13 19:57:20 +0300

    task7 commit at 13.03

---
+++ src/Task7/Main.java
@@ -0,0 +1,9 @@
+package Task7;
+
+public class Main {
+
+    Plane.Wing rightWing = new Plane.Wing();
+    Plane.Wing leftWing = new Plane.Wing();
+
+
+}
---
+++ src/Task7/Plane.java
@@ -0,0 +1,16 @@
+package Task7;
+
+public class Plane {
+
+    public static class Wing {
+        int wingWeight;
+
+        void setWingWeight(int wingWeight) {
+            this.wingWeight = wingWeight;
+        }
+
+        void getWingWeight() {
+            System.out.println(wingWeight);
+        }
+    }
+}

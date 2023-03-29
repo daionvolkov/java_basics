866d35543ef44067a87507b5e2e788de316802ac
daionvolkov <daionvolkov@gmail.com>
2023-03-29 22:47:17 +0300

    MultiStreams 29.03 third corr

--- src/MultiStreams/Ex2/Main.java
+++ src/MultiStreams/Ex2/Main.java
@@ -1,32 +1,30 @@
 package MultiStreams.Ex2;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Counter count = new Counter();
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();
+        CountDownLatch latch = new CountDownLatch(100);
         Thread[] threads = new Thread[100];
+
         for (int i = 0; i < 100; i++) {
             threads[i] = new Thread(() -> {
                 for (int j = 0; j < 1000; j++) {
-                    synchronized (count) {
-                        count.increment();
+                    synchronized (counter) {
+                        counter.increment();
                     }
                 }
+                latch.countDown();
             });
             threads[i].start();
         }
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-
-        }
-        System.out.println(count.getCount());
+        latch.await();
+        System.out.println( counter.getCount());
     }
 }
 
